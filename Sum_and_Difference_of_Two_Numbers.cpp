a48e12e706d3763d7c6398c45f64a8b832d6f33e
Aman kumar <92974468+AMANKUMAR1020@users.noreply.github.com>
2025-06-04 00:55:11 +0530

    Create Sum_and_Difference_of_Two_Numbers.cpp

---
+++ Sum_and_Difference_of_Two_Numbers.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d %d\n",a+b, a-b);
+    float a1,b1;
+    scanf("%f %f",&a1,&b1);
+    printf("%0.1f %0.1f",a1+b1, a1-b1);
+    return 0;
+}
